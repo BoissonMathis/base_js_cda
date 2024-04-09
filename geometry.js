f8ed955958d330050617c31df85ed31646b16c14
BoissonMathis <math.boiss@laposte.net>
2024-04-09 14:11:38 +0200

    fix exo geometry

--- geometry.js
+++ geometry.js
@@ -6,24 +6,24 @@ let rectangle = {
     unite: "cm",
 }
 
-let perimetre = (rectangle.width * 2) + (rectangle.height * 2);
+rectangle.perimetre = (rectangle.width * 2) + (rectangle.height * 2);
 
-// console.log('Le périmetre du rectangle est de :', perimetre, rectangle.unite);
+// console.log('Le périmetre du rectangle est de :', rectangle.perimetre, rectangle.unite);
 
-let area = rectangle.width * rectangle.height;
+rectangle.area = rectangle.width * rectangle.height;
 
-// console.log("L'air du rectangle est de :", area);
+// console.log("L'air du rectangle est de :", rectangle.area);
 
-let hypotenuse = (rectangle.width * rectangle.width) + (rectangle.height * rectangle.height);
-let rc_hypotenuse = Math.sqrt(hypotenuse);
-let rc_hypotenuse_round = rc_hypotenuse.toFixed(2);
-// console.log("La valeur de sa diagonale est de :", hypotenuse, rectangle.unite, "² soit ", rc_hypotenuse);
-// console.log("On peut l'arrondir a :", rc_hypotenuse_round);
+rectangle.hypotenuse = (rectangle.width * rectangle.width) + (rectangle.height * rectangle.height);
+rectangle.rc_hypotenuse = Math.sqrt(rectangle.hypotenuse);
+rectangle.rc_hypotenuse_round = rectangle.rc_hypotenuse.toFixed(2);
+// console.log("La valeur de sa diagonale est de :", rectangle.hypotenuse, rectangle.unite, "² soit ", rectangle.rc_hypotenuse);
+// console.log("On peut l'arrondir a :", rectangle.rc_hypotenuse_round);
 
-// console.log("X: ", rectangle.x, rectangle.unite);
-// console.log("Y: ", rectangle.x, rectangle.unite);
-// console.log("Perimetre: ", perimetre, rectangle.unite);
-// console.log("Aire: ", area, rectangle.unite + "²");
-// console.log("Diagonal: ", rc_hypotenuse_round, rectangle.unite);
+console.log("X: ", rectangle.x, rectangle.unite);
+console.log("Y: ", rectangle.x, rectangle.unite);
+console.log("Perimetre: ", rectangle.perimetre, rectangle.unite);
+console.log("Aire: ", rectangle.area, rectangle.unite + "²");
+console.log("Diagonal: ", rectangle.rc_hypotenuse_round, rectangle.unite);
 
-console.log("X: ", rectangle.x, rectangle.unite + "\n" + "Y: ", rectangle.x, rectangle.unite + "\n" + "Perimetre: ", perimetre, rectangle.unite + "\n" + "Aire: ", area, rectangle.unite + "²" + "\n" + "Diagonal: ", rc_hypotenuse_round, rectangle.unite);
+// console.log("X: ", rectangle.x, rectangle.unite + "\n" + "Y: ", rectangle.x, rectangle.unite + "\n" + "Perimetre: ", rectangle.perimetre, rectangle.unite + "\n" + "Aire: ", rectangle.area, rectangle.unite + "²" + "\n" + "Diagonal: ", rectangle.rc_hypotenuse_round, rectangle.unite);
