636127307e53b307fededc2c8edc9b856b882233
BoissonMathis <math.boiss@laposte.net>
2024-04-09 11:57:11 +0200

    ajout éxo objet

--- produit_object.js
+++ produit_object.js
@@ -7,5 +7,13 @@ let produit = {
 
 let gain = produit.nombre_vendu * produit.prix;
 let valeur_stock = produit.stock * produit.prix;
+let product_cost = 8;
+let total_cost = (produit.nombre_vendu + produit.stock) * product_cost;
+let benef_per_sell = produit.prix - product_cost;
+let pourc_benef_per_sell = 100 * (benef_per_sell/produit.prix)
 
-console.log("nom : ", produit.nom, ", valeur : ", produit.prix, "€", ", stock : ", produit.stock, ", gain : ", gain, '€.');
+console.log("nom : ", produit.nom, ", valeur : ", produit.prix, "€", ", stock : ", produit.stock, ", gain : ", gain, '€');
+
+console.log('Nous avons engagé :', total_cost + '€')
+console.log("Le bénéfice réalisé par produit est de :", benef_per_sell + "€" )
+console.log("Nous réalisons donc un bénéfice de :", Math.round(pourc_benef_per_sell) + "%")
